bd0a776337258e066fe060d81a3be2ae78a5decd
Dan Persa <dan.persa@gmail.com>
2012-03-29 17:03:00 +0300

    validation for email

--- app/models/edge_auth/identity.rb
+++ app/models/edge_auth/identity.rb
@@ -23,8 +23,8 @@ module EdgeAuth
 	  attr_accessible :email, :password, :password_confirmation, :activation_code
 
 	  validates_presence_of       :email, on: :create
-	  validates_length_of         :email, maximum: 255, on: :create
-	  validates_uniqueness_of     :email, case_sensitive: false, on: :create
+	  validates_length_of         :email, maximum: 255
+	  validates_uniqueness_of     :email, case_sensitive: false
 	  validates_format_of         :email, with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, on: :create
 
 	  validates_presence_of       :password, :if => :should_validate_password?
