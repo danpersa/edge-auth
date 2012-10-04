5d7a5b15f60503bb8b1847000d33105f3d9a21dd
Dan Persa <dan.persa@gmail.com>
2012-10-04 19:18:41 +0300

    fixed email validator

--- app/models/edge_auth/email_format_validator.rb
+++ app/models/edge_auth/email_format_validator.rb
@@ -1,9 +1,11 @@
-class EmailFormatValidator < ActiveModel::EachValidator
-  
-  def validate_each(object, attribute, value)
-    email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    unless value.blank? or value =~ email_regex
-      object.errors[attribute] << (options[:message] || "is not formatted properly") 
+module EdgeAuth
+  class EmailFormatValidator < ActiveModel::EachValidator
+
+    def validate_each(object, attribute, value)
+      email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+      unless value.blank? or value =~ email_regex
+        object.errors[attribute] << (options[:message] || "is not formatted properly") 
+      end
     end
   end
 end
