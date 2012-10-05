2ea8d33b2bc6dfac1940ce4c6ffdf05cb5f56b33
Dan Persa <dan.persa@gmail.com>
2012-10-05 11:44:50 +0300

    isolate namespace

--- lib/edge-auth/engine.rb
+++ lib/edge-auth/engine.rb
@@ -1,6 +1,6 @@
 module EdgeAuth
   class Engine < ::Rails::Engine
-    #isolate_namespace EdgeAuth
+    isolate_namespace EdgeAuth
     config.generators do |g|
       g.test_framework      :rspec, :view_specs => false
       g.fixture_replacement :factory_girl
